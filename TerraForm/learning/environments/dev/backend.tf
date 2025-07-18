72e1a87fe0815141700544f618cb01260229191b
krolikowski80 <t.krolikowski@gmail.com>
2025-07-18 20:12:16 +0200

    initial commit

--- TerraForm/learning/environments/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-tk-dev-20250709222448939700000001"
-    key            = "dev-vpc/terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = "terraform-state-lock-dev"
-  }
-}
